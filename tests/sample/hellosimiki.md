0890c5222a03ddcda876c75df947a3aa7c31837f
Tanky Woo <wtq1990@gmail.com>
2014-06-13 02:54:27 +0800

    Update generator tester

--- tests/sample/hellosimiki.md
+++ tests/sample/hellosimiki.md
@@ -1,3 +1,9 @@
+---
+title: "Getting Started"
+layout: page
+date: 2099-06-02 00:00
+---
+
 # Simiki #
 
 Simiki is a simple wiki framework, written in [Python](https://www.python.org/).
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.realpath(os.path.join(TESTS_ROOT, '..')))
 from simiki.configs import parse_configs
 from simiki.generators import PageGenerator
 
-TEST_INPUT_FILE = "./test_input.md"
+TEST_INPUT_FILE = osp.join(TESTS_ROOT, "sample/hellosimiki.md")
 
 EXPECTED_OUTPUT = "./expected_output.html"
 
@@ -25,11 +25,15 @@ class TestPageGenerator(unittest.TestCase):
         )
 
         configs = parse_configs(self.config_file)
-        self.generator = PageGenerator(configs, ".", TEST_INPUT_FILE)
+        self.generator = PageGenerator(configs, TESTS_ROOT, TEST_INPUT_FILE)
 
-    def test_get_category_and_file(self):
-        category_name, filename = self.generator.get_category_and_file()
-        self.assertEqual((category_name, filename), (u'..', u'test_input.md'))
+#    def test_get_category_and_file(self):
+#        category_name, filename = self.generator.get_category_and_file()
+#        self.assertEqual((category_name, filename), (u'sample', u'hellosimiki.md'))
+
+    def test_get_layout(self):
+        layout = self.generator.get_layout()
+        self.assertEqual(layout, "page")
 
 
 if __name__ == "__main__":
